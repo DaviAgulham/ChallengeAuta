d687aef8fa1c656f69047f18d9a47999c62eab44
Davi Agulham <74396578+DaviAgulham@users.noreply.github.com>
2024-08-05 02:41:05 -0300

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrT93415aVlari5IIho7eKHmgfbkbFbjM",
-    authDomain: "car-catalog-fb3e4.firebaseapp.com",
-    projectId: "car-catalog-fb3e4",
-    storageBucket: "car-catalog-fb3e4.appspot.com",
-    messagingSenderId: "51248305111",
-    appId: "1:51248305111:web:a08a7a576c817c518bb192"
+    apiKey: "**********************************",
+    authDomain: "**********************************",
+    projectId: "**********************************",
+    storageBucket: "**********************************",
+    messagingSenderId: "**********************************",
+    appId: "**********************************"
 };
 
 const app = initializeApp(firebaseConfig);
